d4804b3752496ddabe62e65d9ba1fff8a48c7931
Lucas F <lucas_fillon@proton.me>
2025-08-04 08:37:59 +0200

    Update main hooks and endpoints declaration based on refactor

--- src/dge-endpoints/index.ts
+++ src/dge-endpoints/index.ts
@@ -1,62 +1,38 @@
-import {defineEndpoint} from '@directus/extensions-sdk';
-import {InvalidPayloadError} from "@directus/errors";
-import {DgeEdition, DgeTables} from "../common/types-databases";
+import { defineEndpoint } from '@directus/extensions-sdk';
+import { dgeEditionAddItemEndpointCb } from "../common/guide_edition";
+import {
+    dgeDynConfCollectionTrackingEnabledGetEndpoint, dgeDynConfCollectionTrackingEnabledPostEndpoint,
+    dgeDynConfCollectionTypeGetEndpoint,
+    dgeDynConfCollectionTypePostEndpoint
+} from "../common/dynamic_configuration";
 
 export default defineEndpoint({
     id: "dge",
     handler: async (router, context) => {
-        const db = context.database;
-        const {ItemsService} = context.services;
+        console.log("DGE - Endpoints configuration...");
 
-
-        router.get('/edition/:id/add-item', async (req, res) => {
-            const collection: string = req.query.collection;
-            const itemId: number = req.query.itemId;
-            const itemIdColumn: string = req.query.itemIdColumn ? req.query.itemIdColumn : 'id';
-            const editionId: number = req.params.id;
-
-            const dgeEditionContentService = new ItemsService('dge_edition_content', {
-                knex: db,
-                schema: await context.getSchema(),
-                accountability: {admin: true}
-            });
-
-            // Check arguments
-            const edition: DgeEdition = await db(DgeTables.EDITION).where({id: editionId}).first();
-            if (!edition) {
-                const err = new InvalidPayloadError({reason: "Edition doesn't exist"});
-                res.status(err.status).send(err);
-                return;
-            }
-
-            if (!collection) {
-                const err = new InvalidPayloadError({reason: "Missing 'collection' argument in query"});
-                res.status(err.status).send(err);
-                return;
-            }
-
-            if (!itemId) {
-                const err = new InvalidPayloadError({reason: "Missing 'itemId' argument in query"});
-                res.status(err.status).send(err);
-                return;
-            }
-
-            // Do things
-            try {
-                const created = await dgeEditionContentService.createOne({
-                    collection_source: collection,
-                    origin_data: JSON.stringify({[itemIdColumn]: itemId}),
-                    origin_data_column_id: itemIdColumn,
-                    dge_edition: editionId,
-                });
-                console.log(`${created}`);
-            } catch (e: any) {
-                res.status(e.status).send(e);
-                return;
-            }
-
-            // Prepare answer
-            res.send({message: `adding ${itemId} from ${collection} to ${editionId}`})
+        /** Guide edition **/
+        router.get('/edition/:id/add-item', (req, res) => {
+            return dgeEditionAddItemEndpointCb(req, res, context);
         });
+
+        /** Dynamic configuration **/
+        router.get('/collection/:id/type', (req, res) => {
+            return dgeDynConfCollectionTypeGetEndpoint(req, res, context);
+        });
+
+        router.post('/collection/:id/type', (req, res) => {
+            return dgeDynConfCollectionTypePostEndpoint(req, res, context);
+        });
+
+        router.get('/collection/:id/tracking_enabled', (req, res) => {
+            return dgeDynConfCollectionTrackingEnabledGetEndpoint(req, res, context);
+        });
+
+        router.post('/collection/:id/tracking_enabled', (req, res) => {
+            return dgeDynConfCollectionTrackingEnabledPostEndpoint(req, res, context);
+        });
+
+        console.log("DGE - Endpoints configuration done.")
     }
 });
